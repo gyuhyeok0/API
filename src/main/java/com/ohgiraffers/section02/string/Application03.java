3cf46c17e29bcdc24f1c44b8ddeadf4924bb0045
pyunghun <564564q@naver.com>
2024-02-27 13:01:59 +0900

    123

---
+++ src/main/java/com/ohgiraffers/section02/string/Application03.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.section02.string;
+
+import java.util.StringTokenizer;
+
+public class Application03 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. 문자열 분리에 대해 이해하고 적용 할 수 있다.
+            필기.
+                문자열을 특정 구분자로 하여 분리한 문자열을 반환하는 기능을 한다.
+                split() - 공백 문자열도 포함
+                StringTokenizer : 공백 문자열 무시
+         */
+
+        String emp1 = "100/홍길동/서울/영업부";  // 모든 값 존재
+        String emp2 = "200/유관순//총무부";  //주소 없음
+
+        String[] empArr1 = emp1.split("/");
+        String[] empArr2 = emp2.split("/");
+        System.out.println("=====");
+        for (int i = 0; i < empArr1.length; i++) {
+            System.out.println(empArr1[i]);
+        }
+        for (int i = 0; i < empArr2.length; i++) {
+            System.out.println(empArr2[i]);
+        }
+
+        StringTokenizer str1 = new StringTokenizer(emp1,"/");
+        StringTokenizer str2 = new StringTokenizer(emp2,"/");
+
+        while (str1.hasMoreTokens()){
+            System.out.println("str1 :" + str1.nextToken());
+        }
+        // hasMore - 다음 값이 있는 지 확인
+        while (str2.hasMoreTokens()){
+                        // token(구분자) 를 기준으로 출력
+            System.out.println("str2 :" + str2.nextToken());
+        }
+
+    }
+}
+
---
+++ src/main/java/com/ohgiraffers/section02/string/Application04.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section02.string;
+
+public class Application04 {
+
+    /*수업목표. 이스케이프(escape) 문자에 대해 이해하고 적용할 수 있다.
+        필기.
+            문자열 내에서 사용하는 문자 중 특수문자를 표현하거나 특수기능을 사용할 때 사용.
+            \n : 개행
+            \t : 탭
+            \' : 작은 따옴표
+            \" : 큰 따옴표
+            \\ : 역슬래쉬 표
+     */
+    public static void main(String[] args) {
+
+        // 개ㅐ행
+        System.out.println("안녕하세요. \n 저는 홍길동 입니다.");
+        System.out.println("안녕하세요. \t 저는 홍길동 입니다.");
+        System.out.println("안녕하세요. \' 저는 홍길동 입니다.");
+        System.out.println("안녕하세요. \" 저는 홍길동 입니다.");
+
+    }
+
+
+}
